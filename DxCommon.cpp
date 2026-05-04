572441782bd27a3bc61258409f479744e54de1ef
hikari <hikari39764@gmail.com>
2026-05-04 21:05:58 +0900

    fin

--- DxCommon.cpp
+++ DxCommon.cpp
@@ -144,7 +144,7 @@ void DXCommon::ReportLiveObjects() {
 
 	HRESULT hr = DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug));
 	if (SUCCEEDED(hr)) {
-		Log("========== DXGI Live Object Report Start ==========\n");
+		Log("========== DXGI Live Object Report Start!!!!!!!!!!!!!!!!!!!!!!!!!! ==========\n");
 
 		// DXGI全体の生存オブジェクトを出力する
 		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
@@ -155,7 +155,7 @@ void DXCommon::ReportLiveObjects() {
 		// D3D12関連の生存オブジェクトを出力する
 		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
 
-		Log("========== DXGI Live Object Report End ==========\n");
+		Log("========== DXGI Live Object Report End !!!!!!!!!!!!!!!!!!!!!==========\n");
 
 		debug->Release();
 		debug = nullptr;
